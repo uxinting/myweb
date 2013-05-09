d8786f8c020bc9f09b1d869f892abcf2afef0947
uxinting <wu.xinting@hotmail.com>
2013-05-09 11:35:40 +0800

    articless

--- article/views.py
+++ article/views.py
@@ -1,6 +1,23 @@
 #-*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
+from xinting import settings
+import os
+
+ARTICLE = os.path.join(settings.MEDIA_ROOT, 'article')
 
 def article(request):
     title = 'Article'
+    articles = []
+    if request.GET.get('title', None) is None:
+        for t in os.listdir(ARTICLE):
+            path = os.path.join(ARTICLE, t)
+            contents = []
+            contents.append(open(path).readline())
+            articles.append({'title': t, 'contents': contents})
+    else:
+        t = request.GET.get('title', None).encode('utf-8')
+        path = os.path.join(ARTICLE, t)
+        contents = open(path).readlines()
+        articles.append({'title': t, 'contents': contents})
+        all = True
     return render_to_response('article/article.html', locals())
--- xinting/static/js/base.js
+++ xinting/static/js/base.js
@@ -1,7 +1,15 @@
 $(function() {
-	$('#space').css({'height': '20px', 'display': 'none'});
 	setTimeout(function() {
-		$('#topNav').slideUp();
-		$('#space').slideDown();
+		$('.breadcrumbs').slideUp();
 	}, 2000)
+	
+	$('#topNav').mouseover(function() {
+		$('.breadcrumbs').slideDown();
+	});
+	
+	$('#topNav').mouseleave(function() {
+		setTimeout(function() {
+			$('.breadcrumbs').slideUp();
+		}, 2000)
+	});
 })
