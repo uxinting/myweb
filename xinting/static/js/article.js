97b18cd06f9ec7f3e1675b15fa8c78c2ddbc9476
wuxinting <wu.xinting@hotmail.com>
2013-05-13 20:30:05 +0800

    moreduoshuo

--- xinting/static/js/article.js
+++ xinting/static/js/article.js
@@ -1,12 +1,32 @@
 $(function() {
-	$('.icon-comments').click(function() {
-		$($('body').children('div')[0])
-		.removeClass('small-7');
+	$('#comments').click(function() {
+		var offset = $('#article').height();
+		var id = setInterval(moveTop, 10);
+		$(document).scrollTop(10);
 		
-		$('.article').addClass('small-7')
-		
-		$($('.article').next('div'))
-		.removeClass('hide')
-		.addClass('comment');
+		function moveTop() {
+			$(document).scrollTop($(document).scrollTop() * 1.1);
+			if ($(document).scrollTop() > offset)
+				clearInterval(id);
+		}
 	});
+	
+	$(document).scroll(function() {
+		if ($(this).scrollTop() > 0) {
+			$('.icon-backtop').show();
+		} else {
+			$('.icon-backtop').hide();
+		}
+	});
+	
+	$('.icon-backtop').click(function() {
+		var id = setInterval(moveTop, 10);
+		function moveTop() {
+			$(document).scrollTop($(document).scrollTop() / 1.1);
+			if ($(document).scrollTop() < 1)
+				clearInterval(id);
+		}
+	});
+	
+	
 })
