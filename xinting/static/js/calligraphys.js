749cd04aac2ca8217bc4f6a5199c50afd2f075c2
wuxinting <wu.xinting@hotmail.com>
2013-05-18 20:15:05 +0800

    carousel

--- xinting/static/js/calligraphys.js
+++ xinting/static/js/calligraphys.js
@@ -1,8 +1,15 @@
 $(function(){
+	$('.fade-').hide();
+
 	$(".thumbnail").find('img').click(function() {
 		var s = '[src="' + $(this).attr('src') + '"]';
 		$(".carousel-inner").find(s).parent().addClass('active');
+		$('.fade-').show();
 	});
+	
+	$('.close').click(function() {
+		$('.fade-').hide();
+	})
 	
 	$("[href='/calligraphy']").addClass('active');
 });
