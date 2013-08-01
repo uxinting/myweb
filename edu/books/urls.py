b0ee37c29dba11c573cd63bbd0d1ca729f0e976e
新庭 <@Xinting>
2013-08-01 20:54:06 +0800

    books -> chapters -> chapter show

--- edu/books/urls.py
+++ edu/books/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('books.views',
                        url(r'^$', 'Books'),
+                       url(r'^/(\d+)/$', 'Chapters'),
+                       url(r'^/(\d+)/chapter/(\d+)/$', 'BookChapter'),
                        url(r'share', 'Share'),
                        )
--- edu/books/views.py
+++ edu/books/views.py
@@ -1,16 +1,17 @@
 #-*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
-from books.utils import save_file_from_request, get_chapters
+from books.utils import save_file_from_request, get_chapters, get_save_folder
 from django.contrib.auth.decorators import login_required
-from books.models import Book
+from books.models import Book, Chapter
+from django.core.exceptions import ObjectDoesNotExist
 
 def Books(request):
     title = u'著作'
     try:
         index = request.GET.get('index', None)
         if not index:
-            return HttpResponseRedirect('/?index=0')
+            return HttpResponseRedirect('/books?index=0')
         
         index = int(index)
         option = request.GET.get('option', None)
@@ -23,10 +24,29 @@ def Books(request):
         else:pass
 
         books = Book.objects.all()[index : index+10]
+        counts = range(Book.objects.count() / 10)
         reader = request.user.reader
     except Exception, e:
         print e
     return render_to_response('books/books.html', locals())
+
+def Chapters(request, bookId):
+    try:
+        book = Book.objects.get(id=bookId)
+        chapters = Chapter.objects.get(book=book)
+    except ObjectDoesNotExist:
+        print "book is not exist"
+    return render_to_response('books/chapters.html', locals())
+
+def BookChapter(request, bookId, chapterId):
+    try:
+        book = Book.objects.get(id=bookId)
+        import os
+        path = os.path.join(get_save_folder(), repr(book.id))
+        lines = open(path).readlines()
+    except Exception, e:
+        print e
+    return render_to_response('books/article.html', locals())
 
 @login_required
 def Share(request):
@@ -42,6 +62,6 @@ def Share(request):
         except Exception, e:
             error['status'] = False
             error['msg'] = e
-        print error
+
         import json
         return HttpResponse(json.dumps(error), 'json')
